d014f5f20d17c2b09f052761d14de81252625a88
Josh <leong.jo@gmail.com>
2017-10-25 15:28:47 -0600

    got email send working

--- routes/index.js
+++ routes/index.js
@@ -11,11 +11,21 @@ router.get('/', function(req, res, next) {
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
-  to: 'leong.jo@gmail.com',
+  to: 'andfwas@gmail.com',
   from: 'account-verification@waitticket.com',
   subject: 'test',
   text: 'and easy to do anywhere, even with Node.js',
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  id: "0668feb1-5667-496b-8a8e-87ec97cb66c4",
+  template_id: "0668feb1-5667-496b-8a8e-87ec97cb66c4",
+  active: 1,
+  name: "usercreation",
+  html_content: "<%body%>",
+  plain_content: "<%body%>",
+  subject: "<%subject%>",
+  editor: "code",
+  updated_at: "2014-03-19 18:56:33"
+
 };
 sgMail.send(msg);
 
